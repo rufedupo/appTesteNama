6b1dda275daa59ad8db5bf3fbcfafa3e626bb728
Sidus <sidus.master@gmail.com>
2020-10-20 12:49:12 -0300

    Adição de receita bruta total

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,5 +1,12 @@
 class PurchasesController < ApplicationController
     def index
         @purchases = Purchase.all
+
+        @purchaseTotal = 0.00
+
+        @purchases.each do |purchase|
+            total = purchase.quantity * purchase.product.price
+            @purchaseTotal += total
+        end
     end
 end
